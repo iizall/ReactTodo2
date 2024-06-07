639f4920b27e17465169bfce454d312ec4e73c2e
izal <izal@gmail.com>
2024-06-07 21:44:46 +0700

    add delete fitur

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const TodoItem = ({ todo, toggleCompleted }) => {
+const TodoItem = ({ todo, toggleCompleted, deleteTodo }) => {
   const getTodoTitleStyle = () => {
     if (todo.completed === true) {
       return { textDecoration: 'line-through' }
@@ -21,6 +21,10 @@ const TodoItem = ({ todo, toggleCompleted }) => {
         onChange={() => toggleCompleted(todo.id)}
       />
       <p style={getTodoTitleStyle()}>{todo.title}</p>
+      <button 
+        style={styles.button}
+        onClick={() => deleteTodo(todo.id)}
+      >x</button>
     </div>
   )
 }
@@ -29,16 +33,24 @@ const styles = {
   todoItem: {
     border: '2px solid #f4f4f4',
     fontSize: '24px',
-    // Tambahkan styles di bawah ini
     display: 'flex',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    padding: '0 20px',
   },
-  // Tambahkan styles di bawah ini
   checkbox: {
-    marginRight: '10px',
     height: '18px',
     width: '18px',
+  },
+  button: {
+    backgroundColor: '#BB0000',
+    color: '#fff',
+    height: '30px',
+    width: '30px',
+    borderRadius: '100%',
+    border: 'none',
+    cursor: 'pointer',
+    fontSize: '16px',
   },
 }
 
